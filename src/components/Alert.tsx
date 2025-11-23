e7c1797346ef2f0259250c8d7450185facf9068c
Kashish Dhirana <95865697+KashishDhirana@users.noreply.github.com>
2025-11-23 23:35:23 +0530

    Biome format

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,19 +1,19 @@
 import { Info, X } from "lucide-react";
 
 export default function Alert() {
-  // const message = "This is important";
-  const message = "";
+	// const message = "This is important";
+	const message = "";
 
-  if (!message) return null;
+	if (!message) return null;
 
-  return (
-    <div className="bg-gray-600">
-      <div className="px-2 w-full h-10 flex items-center justify-between gap-x-2 bg-red-600/30 border border-red-600 text-red-500">
-        <div className="flex items-center gap-x-2">
-          <Info size={16} /> {message}
-        </div>
-        <X />
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-gray-600">
+			<div className="px-2 w-full h-10 flex items-center justify-between gap-x-2 bg-red-600/30 border border-red-600 text-red-500">
+				<div className="flex items-center gap-x-2">
+					<Info size={16} /> {message}
+				</div>
+				<X />
+			</div>
+		</div>
+	);
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,9 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import type { QueryClient } from "@tanstack/react-query";
 import {
-  createRootRouteWithContext,
-  HeadContent,
-  Scripts,
+	createRootRouteWithContext,
+	HeadContent,
+	Scripts,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import Header from "../components/Header";
@@ -13,65 +13,65 @@ import appCss from "../styles.css?url";
 import Alert from "../components/Alert";
 
 interface MyRouterContext {
-  queryClient: QueryClient;
+	queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  head: () => ({
-    meta: [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      {
-        title: "TanStack Start Starter",
-      },
-    ],
-    links: [
-      {
-        rel: "stylesheet",
-        href: appCss,
-      },
-    ],
-  }),
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "TanStack Start Starter",
+			},
+		],
+		links: [
+			{
+				rel: "stylesheet",
+				href: appCss,
+			},
+		],
+	}),
 
-  shellComponent: RootDocument,
-  notFoundComponent: () => (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-2xl">Page Not Found</p>
-    </div>
-  ),
+	shellComponent: RootDocument,
+	notFoundComponent: () => (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+			<h1 className="text-6xl font-bold mb-4">404</h1>
+			<p className="text-2xl">Page Not Found</p>
+		</div>
+	),
 });
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        <Header />
-        <Alert />
-        {children}
-        <TanStackDevtools
-          config={{
-            position: "bottom-right",
-          }}
-          plugins={[
-            {
-              name: "Tanstack Router",
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-            TanStackQueryDevtools,
-            StoreDevtools,
-          ]}
-        />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				<Header />
+				<Alert />
+				{children}
+				<TanStackDevtools
+					config={{
+						position: "bottom-right",
+					}}
+					plugins={[
+						{
+							name: "Tanstack Router",
+							render: <TanStackRouterDevtoolsPanel />,
+						},
+						TanStackQueryDevtools,
+						StoreDevtools,
+					]}
+				/>
+				<Scripts />
+			</body>
+		</html>
+	);
 }
